e267d25c70ef0dd6eb2e2f3605847828664edee0
Tucaliuc Agnes Monalisa <43825717+monalisaven@users.noreply.github.com>
2021-11-19 11:41:02 +0200

    Update AddLatexCommand.java

--- LatexEditorProject-2022/src/controller/commands/AddLatexCommand.java
+++ LatexEditorProject-2022/src/controller/commands/AddLatexCommand.java
@@ -11,13 +11,10 @@ public class AddLatexCommand implements Command  {
 	private String text;
 	
 	public AddLatexCommand() {
-//		super();
-//		this.versionsManager = versionsManager;
 		versionsManager = VersionsManager.getInstance();
 		latexEditorView = LatexEditorView.getInstance();
 		
 	}
-
 
 	@Override
 	public void execute() {
@@ -34,62 +31,46 @@ public class AddLatexCommand implements Command  {
 		}
 		currentDocument.setContents(text);
 	}
+	
 //	public void editContents(String type) {
 //		String contents = editorPane.getText();
 //		String before = contents.substring(0, editorPane.getCaretPosition());
 //		String after = contents.substring(editorPane.getCaretPosition());
+//		String [] selectType = { "chapter", "section", "subsection", "subsubsection", "enumerate", "itemize", "table", "figure"};
+//		String [] input = {"\n\\chapter{...}", "\n\\section{...}", "\n\\subsection{...}",  "\n\\subsubsection{...}",
+//				"\\begin{enumerate}\n"+
+//				"\\item ...\n"+
+//				"\\item ...\n"+
+//				"\\end{enumerate}\n",
+//				//itemize
+//				"\\begin{itemize}\n"+
+//				"\\item ...\n"+
+//				"\\item ...\n"+
+//				"\\end{itemize}\n",
+//				//table
+//				"\\begin{table}\n"+
+//				"\\caption{....}\\label{...}\n"+
+//				"\\begin{tabular}{|c|c|c|}\n"+
+//				"\\hline\n"+
+//				"... &...&...\\\\\n"+
+//				"... &...&...\\\\\n"+
+//				"... &...&...\\\\\n"+
+//				"\\hline\n"+
+//				"\\end{tabular}\n"+
+//				"\\end{table}\n",
+//				//figure
+//				"\\begin{figure}\n"+
+//				"\\includegraphics[width=...,height=...]{...}\n"+
+//				"\\caption{....}\\label{...}\n"+
+//				"\\end{figure}\n"};
 //		
-//		if(type.equals("chapter")) {
-//			contents = before + "\n\\chapter{...}"+"\n"+after;
-//		}
-//		else if(type.equals("section")) {
-//			contents = before + "\n\\section{...}"+"\n"+after;
-//		}
-//		else if(type.equals("subsection")) {
-//			contents = before + "\n\\subsection{...}"+"\n"+after;
-//		}
-//		else if(type.equals("subsubsection")) {
-//			contents = before + "\n\\subsubsection{...}"+"\n"+after;
-//		}
-//		else if(type.equals("enumerate")) {
-//			contents = before + 
-//					"\\begin{enumerate}\n"+
-//					"\\item ...\n"+
-//					"\\item ...\n"+
-//					"\\end{enumerate}\n"+after;
-//		}
-//		else if(type.equals("itemize")) {
-//			contents = before + 
-//					"\\begin{itemize}\n"+
-//					"\\item ...\n"+
-//					"\\item ...\n"+
-//					"\\end{itemize}\n"+after;
-//		}
-//		else if(type.equals("table")) {
-//			contents = before + 
-//					"\\begin{table}\n"+
-//					"\\caption{....}\\label{...}\n"+
-//					"\\begin{tabular}{|c|c|c|}\n"+
-//					"\\hline\n"+
-//					"... &...&...\\\\\n"+
-//					"... &...&...\\\\\n"+
-//					"... &...&...\\\\\n"+
-//					"\\hline\n"+
-//					"\\end{tabular}\n"+
-//					"\\end{table}\n"+after;
-//		}
-//		else if(type.equals("figure")) {
-//			contents = before + 
-//					"\\begin{figure}\n"+
-//					"\\includegraphics[width=...,height=...]{...}\n"+
-//					"\\caption{....}\\label{...}\n"+
-//					"\\end{figure}\n"+after;
-//;
+//		for(int i = 0; i < selectType.length; i++) {
+//			if(type.equals(selectType[i])){
+//				contents = before + input[i] + "\n"+after;
+//			}
 //		}
 //		latexEditorView.setText(contents);
 //		latexEditorView.getController().enact("addLatex");
 //		editorPane.setText(contents);
 //	}
-
-	
 }
