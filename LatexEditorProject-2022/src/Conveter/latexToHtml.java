973039ef8cffa726687909dc99f925bf4b4531ea
johnprif <56134761+johnprif@users.noreply.github.com>
2021-12-04 17:55:12 +0200

    it works for all 3 section table

--- LatexEditorProject-2022/src/Conveter/latexToHtml.java
+++ LatexEditorProject-2022/src/Conveter/latexToHtml.java
@@ -47,28 +47,28 @@ public class latexToHtml
 			for(int i=0; i<latexLines.length-1; i++)
 			{
 				String[] temp = latexLines[i].split("}");
-				if(latexLines[i].contains("chapter") && !latexLines[i].contains("chapter*"))
+				if(latexLines[i].contains("chapter") && !latexLines[i].contains("chapter*")) //chapter
 				{
 					htmlContents = htmlContents + "<h1 id=\""+temp[0].substring(8, temp[0].length())+"\">"+temp[0].substring(8, temp[0].length())+"</h1>" +"\n";
-				}else if(latexLines[i].contains("section") && !latexLines[i].contains("section*") && !latexLines[i].contains("subsection") && !latexLines[i].contains("subsubsection"))
+				}else if(latexLines[i].contains("section") && !latexLines[i].contains("section*") && !latexLines[i].contains("subsection") && !latexLines[i].contains("subsubsection")) //section
 				{
 					htmlContents = htmlContents + "<h2 id=\""+temp[0].substring(8, temp[0].length())+"\">"+temp[0].substring(8, temp[0].length())+"</h2>" +"\n";
-				}else if(latexLines[i].contains("subsection") && !latexLines[i].contains("subsubsection"))
+				}else if(latexLines[i].contains("subsection") && !latexLines[i].contains("subsubsection")) //subsection
 				{
 					htmlContents = htmlContents + "<h3 id=\""+temp[0].substring(11, temp[0].length())+"\">"+temp[0].substring(11, temp[0].length())+"</h3>" +"\n";
-				}else if(latexLines[i].contains("subsubsection"))
+				}else if(latexLines[i].contains("subsubsection")) //subsubsection
 				{
 					htmlContents = htmlContents + "<h4 id=\""+temp[0].substring(14, temp[0].length())+"\">"+temp[0].substring(14, temp[0].length())+"</h4>" +"\n";
-				}else if(latexLines[i].contains("subparagraph"))
+				}else if(latexLines[i].contains("subparagraph")) //subparagraph
 				{
 					htmlContents = htmlContents + "<h5 id=\""+temp[0].substring(13, temp[0].length())+"\">"+temp[0].substring(13, temp[0].length())+"</h5>" +"\n";
-				}else if(latexLines[i].contains("chapter*"))
+				}else if(latexLines[i].contains("chapter*")) //other chapter
 				{
 					htmlContents = htmlContents + "<h1 class=\"unnumbered\" id=\""+temp[0].substring(9, temp[0].length())+"\">"+temp[0].substring(9, temp[0].length())+"</h1>" +"\n";
-				}else if(latexLines[i].contains("section*"))
+				}else if(latexLines[i].contains("section*")) //other section
 				{
 					htmlContents = htmlContents + "<h1 class=\"unnumbered\" id=\""+temp[0].substring(9, temp[0].length())+"\">"+temp[0].substring(9, temp[0].length())+"</h1>" +"\n";
-				}else if(latexLines[i].contains("par"))
+				}else if(latexLines[i].contains("par")) // new paragraph
 				{
 					htmlContents = htmlContents + "<p id=\""+temp[0].substring(4, temp[0].length())+"\">"+temp[0].substring(4, temp[0].length())+"</p>" +"\n";
 				}else if(latexLines[i].contains("begin{enumerate}"))
@@ -89,9 +89,21 @@ public class latexToHtml
 				}else if(latexLines[i].contains("end{itemize}"))
 				{
 					htmlContents = htmlContents + "</ul>" +"\n";
-				}else if(latexLines[i].contains("text"))
+				}else if(latexLines[i].contains("text") && !latexLines[i].contains("textit{") && !latexLines[i].contains("textbf{") && !latexLines[i].contains("texttt{")) //definition
 				{
 					htmlContents = htmlContents + "<dd id=\""+latexLines[i].substring(5, latexLines[i].length())+"\">"+latexLines[i].substring(5, latexLines[i].length())+"</dd>" +"\n";
+				}else if(latexLines[i].contains("emph{")) //emphasis
+				{
+					htmlContents = htmlContents + "<em id=\""+temp[0].substring(5, temp[0].length())+"\">"+temp[0].substring(5, temp[0].length())+"</em>" +"\n";
+				}else if(latexLines[i].contains("textit{")) //italic
+				{
+					htmlContents = htmlContents + "<i id=\""+temp[0].substring(7, temp[0].length())+"\">"+temp[0].substring(7, temp[0].length())+"</i>" +"\n";
+				}else if(latexLines[i].contains("textbf{")) //bold
+				{
+					htmlContents = htmlContents + "<b id=\""+temp[0].substring(7, temp[0].length())+"\">"+temp[0].substring(7, temp[0].length())+"</b>" +"\n";
+				}else if(latexLines[i].contains("texttt{")) //fixed with
+				{
+					htmlContents = htmlContents + "<tt id=\""+temp[0].substring(7, temp[0].length())+"\">"+temp[0].substring(7, temp[0].length())+"</tt>" +"\n";
 				}
 			}
 		}
