66f017d4e56d0d8ee68ac53e50abfdce5ec8ceb6
johnprif <56134761+johnprif@users.noreply.github.com>
2021-12-08 20:47:29 +0200

    new names

--- LatexEditorProject-2022/src/TestPacket/SaveAsTest_tex_to_html.java
+++ LatexEditorProject-2022/src/TestPacket/SaveAsTest_tex_to_html.java
@@ -9,23 +9,25 @@ import java.util.Scanner;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import Conveter.latexToHtml;
 import model.Document;
 import view.LatexEditorView;
 
-class SaveAsTest {
-	
+class SaveAsTest_tex_to_html {
+
 	private static Document document;
 	private static LatexEditorView latexEditorView;
+	private static latexToHtml converter;
 	private static String expected;
-	private static String fileNameWithPath = "C:\\Users\\Joanis Prifti\\Desktop\\test.tex";
-	
+	private static String latexBeforeConverted;
+	private static String fileNameWithPath = "C:\\Users\\Joanis Prifti\\Desktop\\test.html";
 	@BeforeAll
 	public static void initialize() 
 	{
 		latexEditorView = LatexEditorView.getInstance();
 		document = new Document();
 		
-		expected = "\\documentclass[11pt,a4paper]{report}\r\n"
+		latexBeforeConverted = "\\documentclass[11pt,a4paper]{report}\r\n"
 				+ "\r\n"
 				+ "\\begin{document}\r\n"
 				+ "\\title{Report Template: How to Structure a LaTeX Document}\r\n"
@@ -53,7 +55,17 @@ class SaveAsTest {
 				+ "\\end{document}\r\n"
 				+ "";
 		
-		document.setContents(expected);
+		expected = "<h1 id=\"AdvancedLatexEditorView\">AdvancedLatexEditorView</h1>\r\n"
+				+ "<h2 id=\"PRIFTI JOANIS 3321\">PRIFTI JOANIS 3321</h2>\r\n"
+				+ "<h2 id=\"TUCALIUC AGNES MONALISA 3346\">TUCALIUC AGNES MONALISA 3346</h2>\r\n"
+				+ "<h2 id=\"Section Title.....\">Section Title.....</h2>\r\n"
+				+ "<h1 id=\"....\">....</h1>\r\n"
+				+ "<h1 id=\"WE PASS THE LESSON\">WE PASS THE LESSON</h1>\r\n"
+				+ "<h1 class=\"unnumbered\" id=\"References\">References</h1>\r\n";
+		
+		converter = new latexToHtml(latexBeforeConverted);
+		document.setContents(converter.getHtmlContents());
+
 		latexEditorView.setCurrentDocument(document);
 		latexEditorView.setFilename(fileNameWithPath);
 		document.save(latexEditorView.getFilename());
@@ -72,4 +84,5 @@ class SaveAsTest {
 	    }
 	    assertEquals(actualContent, expected);
 	}
+
 }
--- LatexEditorProject-2022/src/TestPacket/SaveAsTest_tex_to_tex.java
+++ LatexEditorProject-2022/src/TestPacket/SaveAsTest_tex_to_tex.java
@@ -9,25 +9,23 @@ import java.util.Scanner;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import Conveter.latexToHtml;
 import model.Document;
 import view.LatexEditorView;
 
-class latexToHtmlTest {
-
+class SaveAsTest_tex_to_tex {
+	
 	private static Document document;
 	private static LatexEditorView latexEditorView;
-	private static latexToHtml converter;
 	private static String expected;
-	private static String latexBeforeConverted;
-	private static String fileNameWithPath = "C:\\Users\\Joanis Prifti\\Desktop\\test.html";
+	private static String fileNameWithPath = "C:\\Users\\Joanis Prifti\\Desktop\\test.tex";
+	
 	@BeforeAll
 	public static void initialize() 
 	{
 		latexEditorView = LatexEditorView.getInstance();
 		document = new Document();
 		
-		latexBeforeConverted = "\\documentclass[11pt,a4paper]{report}\r\n"
+		expected = "\\documentclass[11pt,a4paper]{report}\r\n"
 				+ "\r\n"
 				+ "\\begin{document}\r\n"
 				+ "\\title{Report Template: How to Structure a LaTeX Document}\r\n"
@@ -55,17 +53,7 @@ class latexToHtmlTest {
 				+ "\\end{document}\r\n"
 				+ "";
 		
-		expected = "<h1 id=\"AdvancedLatexEditorView\">AdvancedLatexEditorView</h1>\r\n"
-				+ "<h2 id=\"PRIFTI JOANIS 3321\">PRIFTI JOANIS 3321</h2>\r\n"
-				+ "<h2 id=\"TUCALIUC AGNES MONALISA 3346\">TUCALIUC AGNES MONALISA 3346</h2>\r\n"
-				+ "<h2 id=\"Section Title.....\">Section Title.....</h2>\r\n"
-				+ "<h1 id=\"....\">....</h1>\r\n"
-				+ "<h1 id=\"WE PASS THE LESSON\">WE PASS THE LESSON</h1>\r\n"
-				+ "<h1 class=\"unnumbered\" id=\"References\">References</h1>\r\n";
-		
-		converter = new latexToHtml(latexBeforeConverted);
-		document.setContents(converter.getHtmlContents());
-
+		document.setContents(expected);
 		latexEditorView.setCurrentDocument(document);
 		latexEditorView.setFilename(fileNameWithPath);
 		document.save(latexEditorView.getFilename());
@@ -84,5 +72,4 @@ class latexToHtmlTest {
 	    }
 	    assertEquals(actualContent, expected);
 	}
-
 }
